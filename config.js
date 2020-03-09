6a9428b401e5db5ad369166fa5656f0af533c96a
joeyklee <jsphknglee@gmail.com>
2020-03-09 16:57:38 -0400

    adds latest

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-module.exports = {
-    USERNAME: process.env.USERNAME,
-    PASSWORD: process.env.PASSWORD,
-    PORT: process.env.PORT || 3000,
-}
+   module.exports = {
+       USERNAME: process.env.USERNAME,
+       PASSWORD: process.env.PASSWORD,
+       PORT: process.env.PORT || 3000,
+       NEDB_URI: process.env.NEDB_URI || './db/hellos'
+   }
